21f8245ffc6794047d74479dd2b0c6515373bbb4
Jijidici <jdefaye.jijidici@gmail.com>
2014-02-18 18:41:03 +0100

    Clean initial project

--- Classes/AppDelegate.cpp
+++ Classes/AppDelegate.cpp
@@ -1,5 +1,4 @@
 #include "AppDelegate.h"
-#include "HelloWorldScene.h"
 
 USING_NS_CC;
 
@@ -24,11 +23,7 @@ bool AppDelegate::applicationDidFinishLaunching() {
     // set FPS. the default value is 1.0/60 if you don't call this
     director->setAnimationInterval(1.0 / 60);
 
-    // create a scene. it's an autorelease object
-    auto scene = HelloWorld::createScene();
-
-    // run
-    director->runWithScene(scene);
+    
 
     return true;
 }
